be2f4efbc18f94b00e0ef53f658dab33e3ca411b
Larson-Dav <larsonkapita@gmail.com>
2025-03-28 22:48:24 +0200

    Online api is available

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -48,7 +48,7 @@ export default function Board() {
     const fetchData = async () => {
 
       try {
-        const response = await fetch("http://localhost:8000/api/get");
+        const response = await fetch("https://live-server-triomac.onrender.com/api/get");
         const result = await response.json();
         setData(result);
       } catch (err) {
