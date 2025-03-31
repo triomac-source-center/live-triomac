1e923f911a22d92f74ca5a68cc973a35dad9c4f4
Larson-Dav <larsonkapita@gmail.com>
2025-03-31 09:58:30 +0200

    action correction

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -1,7 +1,7 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { WebhookEvent, clerkClient } from '@clerk/nextjs/server'
-import { createUser } from '@/actions/user.action'
+import { createUser } from '../../../../actions/user.action'
 import { NextResponse } from 'next/server'
 
 export async function POST(req: Request) {
