0ee348d46f729c7ec24841f9a2fe5b1cc70640f9
Larson-Dav <larsonkapita@gmail.com>
2025-03-31 10:06:32 +0200

    string updated

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -5,7 +5,7 @@ import { createUser } from '../../../../actions/user.action'
 import { NextResponse } from 'next/server'
 
 export async function POST(req: Request) {
-  const SIGNING_SECRET = process.env.SIGNING_SECRET
+  const SIGNING_SECRET = 'whsec_vh+ggSlWHaNC6GBzOi1L6/CvsR2511sA'
 
   if (!SIGNING_SECRET) {
     throw new Error('Error: Please add SIGNING_SECRET from Clerk Dashboard to .env or .env')
